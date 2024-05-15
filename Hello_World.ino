d3451d400e04836e7d97145aedbff779d18fc217
Tahmidur Rahman Muhtadee <80316577+Tahmid999@users.noreply.github.com>
2024-05-15 15:13:12 +0600

    Update and rename Hello_World to Hello_World.ino

---
+++ Hello_World.ino
@@ -0,0 +1,10 @@
+void setup()
+{
+  Serial.begin(9600); //This line initializes serial communication with a baud rate of 9600 bits per second.
+    Serial.println("Hello World");
+}
+
+void loop()
+{
+  
+}
