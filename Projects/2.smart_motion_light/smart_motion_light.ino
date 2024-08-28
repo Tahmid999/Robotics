cc6be251321fbc580e42daa355dbaa8a206dec03
Tahmidur Rahman Muhtadee <80316577+Tahmid999@users.noreply.github.com>
2024-08-28 11:40:27 +0600

    Update project file smart_motion_light.ino
    Uploaded source code.

--- Projects/2.smart_motion_light/smart_motion_light.ino
+++ Projects/2.smart_motion_light/smart_motion_light.ino
@@ -1,1 +1,25 @@
-// Loading
+int relay = 5;
+int pir = 6;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(5, OUTPUT);
+  pinMode(6, INPUT);
+}
+
+void loop()
+{
+  
+  if(digitalRead(pir)==HIGH)
+  {
+  	Serial.println("Motion Ditected");
+    digitalWrite(relay, HIGH);
+  }
+  else
+  {
+  	Serial.println("Motion not Ditected");
+    digitalWrite(relay, LOW);
+  }
+  
+}
